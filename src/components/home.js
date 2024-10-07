7ae3303a94d54749b05d313000fa40be003532e6
RMTabish <raimuhammadtabish@gmail.com>
2024-10-08 00:13:47 +0500

    home page done

--- src/components/home.js
+++ src/components/home.js
@@ -12,17 +12,34 @@ return (
 
             <h1>Hey! I'M RAI TABISH</h1>
             <p>
-            I'm a Computer Science major graduating in 2025.
-            An aspiring Software engineer with a focus on
+             I'm a Computer Science major graduating in June 2025.<br></br>
+
+             An aspiring Software engineer with a focus on
              data engineering, NLP, and computer vision, always exploring new technologies in software engineering.
 
                 
             </p>
+
+            <a href="/Rai_Tabish_CV_7.pdf" download className="download-btn">Download My Resume</a>
+
         </div>
         <div className="homeright">
             <div className="pf">
                 <img src="/nicatDP.jpeg" alt= 'Rai Tabish'></img>
             </div>
+
+            <div className="social-links">
+                <a href="mailto:raimuhammadtabish@gmail.com" className="social-btn">
+                    <img src="/gmail.png" alt="Email" />
+                </a>
+                <a href="https://github.com/RMTabish" target="_blank" className="social-btn">
+                    <img src="/github.png" alt="GitHub" />
+                </a>
+                <a href="https://linkedin.com/in/rai-muhammad-tabish" target="_blank" className="social-btn">
+                    <img src="/linkedin.png" alt="LinkedIn" />
+                </a>
+                </div>
+
         </div>
 
     </div>
